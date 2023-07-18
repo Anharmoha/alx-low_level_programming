6fdea841b9c436231d5dd81306fefce447635693
Anhar <Anhar4496@gmail.com>
2023-07-18 09:09:54 -0700

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
   * main: - entry point
-  * Return: always 0 (success)
+  * Return: always 1 (success)
   */
 int main(void)
 {
-	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n",stdout);
-	return (0);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
